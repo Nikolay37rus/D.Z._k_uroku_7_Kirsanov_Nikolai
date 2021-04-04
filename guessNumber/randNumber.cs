5dff1c957f2fc20487268afadb790dcf66c49644
Nikolay37rus <nikolayhupahus@mail.ru>
2021-04-04 13:19:04 +0300

    Добавьте файлы проекта.

---
+++ guessNumber/randNumber.cs
@@ -0,0 +1,61 @@
+﻿//2. Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает
+//число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Для
+//ввода данных от человека используется элемент TextBox.
+//Д_З_к_уроку_7_Кирсанов_Николай_Задание_2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessNumber
+{
+    class randNumber
+    {
+        int value;
+        int steps;
+
+        public int Value { get { return this.value; } }
+        public int Steps { get { return this.steps; } }
+
+        public randNumber(int max)
+        {
+            start(max);
+        }
+
+        public void start(int max)
+        {
+            Random rand = new Random();
+            this.value = rand.Next(0, max + 1);
+        }
+
+        public string CheckValue (out bool check, int userAnswer)
+        {
+            if (userAnswer == this.value)
+            {
+                check = true;
+                return $"Поздравляю, вы угадали за {this.steps} ходов!";
+            }
+            else if(userAnswer > this.value)
+            {
+                check = false;
+                steps++;
+                return $"Введённое число большое, попробуйте ещё раз!";
+            }
+            else
+            {
+                check = false;
+                steps++;
+                return $"Введённое число мало, попробуйте ещё раз!";
+            }
+        }
+
+        public void Reset(int max)
+        {
+            start(max);
+            this.steps = 0; 
+        }
+        
+    }
+}
